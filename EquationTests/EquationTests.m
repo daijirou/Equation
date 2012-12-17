e825ef996e572cabef2ebb0d835c8256a107fc98
daijiro <b1036701@gmail.com>
2012-12-17 14:08:39 +0900

    課題3

--- EquationTests/EquationTests.m
+++ EquationTests/EquationTests.m
@@ -174,6 +174,20 @@ Equation* e;
     STAssertEqualsWithAccuracy(-1.0, [e imaginary2], 0.00001, @"imaginary2 error");
 }
 
+// x^2 - (1+3i)x + 3i = 0 をテスト
+// 虚数解 2つの場合。
+- (void)test8
+{
+    e = [[Equation alloc] initWithA:1 b:-(1+3i) c:3i];
+    
+    // STAssertEqualsWithAccuracy は浮動小数点演算をテストするときに使います。
+    // 浮動小数点演算では誤差が発生するので，誤差の範囲内で等しいことを確かめる必要があります。
+    // accuracy とは誤差のことです。
+    STAssertEqualsWithAccuracy(1.0, [e real1], 0.00001, @"real1 error");
+    STAssertEqualsWithAccuracy(0.0, [e real2], 0.00001, @"real2 error");
+    STAssertEqualsWithAccuracy(0.0, [e imaginary1], 0.00001, @"imaginary1 error");
+    STAssertEqualsWithAccuracy(3.0, [e imaginary2], 0.00001, @"imaginary2 error");
+}
 
 
 @end
